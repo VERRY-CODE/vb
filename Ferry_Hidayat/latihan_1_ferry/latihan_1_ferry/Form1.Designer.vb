1cb5328b12b23e3022c2b920198fe9603c06113e
FERRYHIDAYAT <145472042+VERRY-CODE@users.noreply.github.com>
2024-10-04 09:57:06 +0700

    Add files via upload

---
+++ Ferry_Hidayat/latihan_1_ferry/latihan_1_ferry/Form1.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class KALKULATOR
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtlayar = New System.Windows.Forms.TextBox()
+        Me.cmdtambah = New System.Windows.Forms.Button()
+        Me.cmdkurang = New System.Windows.Forms.Button()
+        Me.cmdkali = New System.Windows.Forms.Button()
+        Me.cmdbagi = New System.Windows.Forms.Button()
+        Me.panah = New System.Windows.Forms.Button()
+        Me.cmdbutton7 = New System.Windows.Forms.Button()
+        Me.cmdbutton8 = New System.Windows.Forms.Button()
+        Me.cmdbutton9 = New System.Windows.Forms.Button()
+        Me.cmdhapus = New System.Windows.Forms.Button()
+        Me.cmdbutton4 = New System.Windows.Forms.Button()
+        Me.cmdbutton5 = New System.Windows.Forms.Button()
+        Me.cmdbutton6 = New System.Windows.Forms.Button()
+        Me.cmdsamadengan = New System.Windows.Forms.Button()
+        Me.cmdbutton1 = New System.Windows.Forms.Button()
+        Me.cmdbutton2 = New System.Windows.Forms.Button()
+        Me.cmdbutton3 = New System.Windows.Forms.Button()
+        Me.cmdbutton0 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtlayar
+        '
+        Me.txtlayar.Location = New System.Drawing.Point(12, 12)
+        Me.txtlayar.Multiline = True
+        Me.txtlayar.Name = "txtlayar"
+        Me.txtlayar.Size = New System.Drawing.Size(477, 141)
+        Me.txtlayar.TabIndex = 0
+        '
+        'cmdtambah
+        '
+        Me.cmdtambah.Location = New System.Drawing.Point(12, 172)
+        Me.cmdtambah.Name = "cmdtambah"
+        Me.cmdtambah.Size = New System.Drawing.Size(91, 59)
+        Me.cmdtambah.TabIndex = 1
+        Me.cmdtambah.Text = "+"
+        Me.cmdtambah.UseVisualStyleBackColor = True
+        '
+        'cmdkurang
+        '
+        Me.cmdkurang.Location = New System.Drawing.Point(127, 173)
+        Me.cmdkurang.Name = "cmdkurang"
+        Me.cmdkurang.Size = New System.Drawing.Size(91, 58)
+        Me.cmdkurang.TabIndex = 2
+        Me.cmdkurang.Text = "-"
+        Me.cmdkurang.UseVisualStyleBackColor = True
+        '
+        'cmdkali
+        '
+        Me.cmdkali.Location = New System.Drawing.Point(244, 172)
+        Me.cmdkali.Name = "cmdkali"
+        Me.cmdkali.Size = New System.Drawing.Size(91, 58)
+        Me.cmdkali.TabIndex = 3
+        Me.cmdkali.Text = "X"
+        Me.cmdkali.UseVisualStyleBackColor = True
+        '
+        'cmdbagi
+        '
+        Me.cmdbagi.Location = New System.Drawing.Point(360, 172)
+        Me.cmdbagi.Name = "cmdbagi"
+        Me.cmdbagi.Size = New System.Drawing.Size(60, 58)
+        Me.cmdbagi.TabIndex = 4
+        Me.cmdbagi.Text = "/"
+        Me.cmdbagi.UseVisualStyleBackColor = True
+        '
+        'panah
+        '
+        Me.panah.Location = New System.Drawing.Point(426, 172)
+        Me.panah.Name = "panah"
+        Me.panah.Size = New System.Drawing.Size(63, 58)
+        Me.panah.TabIndex = 5
+        Me.panah.Text = "^"
+        Me.panah.UseVisualStyleBackColor = True
+        '
+        'cmdbutton7
+        '
+        Me.cmdbutton7.Location = New System.Drawing.Point(12, 239)
+        Me.cmdbutton7.Name = "cmdbutton7"
+        Me.cmdbutton7.Size = New System.Drawing.Size(91, 74)
+        Me.cmdbutton7.TabIndex = 6
+        Me.cmdbutton7.Text = "7"
+        Me.cmdbutton7.UseVisualStyleBackColor = True
+        '
+        'cmdbutton8
+        '
+        Me.cmdbutton8.Location = New System.Drawing.Point(127, 239)
+        Me.cmdbutton8.Name = "cmdbutton8"
+        Me.cmdbutton8.Size = New System.Drawing.Size(91, 74)
+        Me.cmdbutton8.TabIndex = 7
+        Me.cmdbutton8.Text = "8"
+        Me.cmdbutton8.UseVisualStyleBackColor = True
+        '
+        'cmdbutton9
+        '
+        Me.cmdbutton9.Location = New System.Drawing.Point(244, 239)
+        Me.cmdbutton9.Name = "cmdbutton9"
+        Me.cmdbutton9.Size = New System.Drawing.Size(91, 74)
+        Me.cmdbutton9.TabIndex = 8
+        Me.cmdbutton9.Text = "9"
+        Me.cmdbutton9.UseVisualStyleBackColor = True
+        '
+        'cmdhapus
+        '
+        Me.cmdhapus.Location = New System.Drawing.Point(360, 239)
+        Me.cmdhapus.Name = "cmdhapus"
+        Me.cmdhapus.Size = New System.Drawing.Size(129, 74)
+        Me.cmdhapus.TabIndex = 9
+        Me.cmdhapus.Text = "C"
+        Me.cmdhapus.UseVisualStyleBackColor = True
+        '
+        'cmdbutton4
+        '
+        Me.cmdbutton4.Location = New System.Drawing.Point(12, 329)
+        Me.cmdbutton4.Name = "cmdbutton4"
+        Me.cmdbutton4.Size = New System.Drawing.Size(91, 69)
+        Me.cmdbutton4.TabIndex = 10
+        Me.cmdbutton4.Text = "4"
+        Me.cmdbutton4.UseVisualStyleBackColor = True
+        '
+        'cmdbutton5
+        '
+        Me.cmdbutton5.Location = New System.Drawing.Point(127, 329)
+        Me.cmdbutton5.Name = "cmdbutton5"
+        Me.cmdbutton5.Size = New System.Drawing.Size(91, 69)
+        Me.cmdbutton5.TabIndex = 11
+        Me.cmdbutton5.Text = "5"
+        Me.cmdbutton5.UseVisualStyleBackColor = True
+        '
+        'cmdbutton6
+        '
+        Me.cmdbutton6.Location = New System.Drawing.Point(244, 329)
+        Me.cmdbutton6.Name = "cmdbutton6"
+        Me.cmdbutton6.Size = New System.Drawing.Size(91, 69)
+        Me.cmdbutton6.TabIndex = 12
+        Me.cmdbutton6.Text = "6"
+        Me.cmdbutton6.UseVisualStyleBackColor = True
+        '
+        'cmdsamadengan
+        '
+        Me.cmdsamadengan.Location = New System.Drawing.Point(360, 329)
+        Me.cmdsamadengan.Name = "cmdsamadengan"
+        Me.cmdsamadengan.Size = New System.Drawing.Size(129, 223)
+        Me.cmdsamadengan.TabIndex = 13
+        Me.cmdsamadengan.Text = "="
+        Me.cmdsamadengan.UseVisualStyleBackColor = True
+        '
+        'cmdbutton1
+        '
+        Me.cmdbutton1.Location = New System.Drawing.Point(12, 411)
+        Me.cmdbutton1.Name = "cmdbutton1"
+        Me.cmdbutton1.Size = New System.Drawing.Size(91, 74)
+        Me.cmdbutton1.TabIndex = 14
+        Me.cmdbutton1.Text = "1"
+        Me.cmdbutton1.UseVisualStyleBackColor = True
+        '
+        'cmdbutton2
+        '
+        Me.cmdbutton2.Location = New System.Drawing.Point(127, 411)
+        Me.cmdbutton2.Name = "cmdbutton2"
+        Me.cmdbutton2.Size = New System.Drawing.Size(91, 74)
+        Me.cmdbutton2.TabIndex = 15
+        Me.cmdbutton2.Text = "2"
+        Me.cmdbutton2.UseVisualStyleBackColor = True
+        '
+        'cmdbutton3
+        '
+        Me.cmdbutton3.Location = New System.Drawing.Point(244, 411)
+        Me.cmdbutton3.Name = "cmdbutton3"
+        Me.cmdbutton3.Size = New System.Drawing.Size(91, 74)
+        Me.cmdbutton3.TabIndex = 16
+        Me.cmdbutton3.Text = "3"
+        Me.cmdbutton3.UseVisualStyleBackColor = True
+        '
+        'cmdbutton0
+        '
+        Me.cmdbutton0.Location = New System.Drawing.Point(12, 494)
+        Me.cmdbutton0.Name = "cmdbutton0"
+        Me.cmdbutton0.Size = New System.Drawing.Size(323, 58)
+        Me.cmdbutton0.TabIndex = 17
+        Me.cmdbutton0.Text = "0"
+        Me.cmdbutton0.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 574)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(64, 20)
+        Me.TextBox1.TabIndex = 18
+        Me.TextBox1.Text = "JUMAT 04 "
+        '
+        'KALKULATOR
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.ClientSize = New System.Drawing.Size(505, 603)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.cmdbutton0)
+        Me.Controls.Add(Me.cmdbutton3)
+        Me.Controls.Add(Me.cmdbutton2)
+        Me.Controls.Add(Me.cmdbutton1)
+        Me.Controls.Add(Me.cmdsamadengan)
+        Me.Controls.Add(Me.cmdbutton6)
+        Me.Controls.Add(Me.cmdbutton5)
+        Me.Controls.Add(Me.cmdbutton4)
+        Me.Controls.Add(Me.cmdhapus)
+        Me.Controls.Add(Me.cmdbutton9)
+        Me.Controls.Add(Me.cmdbutton8)
+        Me.Controls.Add(Me.cmdbutton7)
+        Me.Controls.Add(Me.panah)
+        Me.Controls.Add(Me.cmdbagi)
+        Me.Controls.Add(Me.cmdkali)
+        Me.Controls.Add(Me.cmdkurang)
+        Me.Controls.Add(Me.cmdtambah)
+        Me.Controls.Add(Me.txtlayar)
+        Me.Name = "KALKULATOR"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtlayar As System.Windows.Forms.TextBox
+    Friend WithEvents cmdtambah As System.Windows.Forms.Button
+    Friend WithEvents cmdkurang As System.Windows.Forms.Button
+    Friend WithEvents cmdkali As System.Windows.Forms.Button
+    Friend WithEvents cmdbagi As System.Windows.Forms.Button
+    Friend WithEvents panah As System.Windows.Forms.Button
+    Friend WithEvents cmdbutton7 As System.Windows.Forms.Button
+    Friend WithEvents cmdbutton8 As System.Windows.Forms.Button
+    Friend WithEvents cmdbutton9 As System.Windows.Forms.Button
+    Friend WithEvents cmdhapus As System.Windows.Forms.Button
+    Friend WithEvents cmdbutton4 As System.Windows.Forms.Button
+    Friend WithEvents cmdbutton5 As System.Windows.Forms.Button
+    Friend WithEvents cmdbutton6 As System.Windows.Forms.Button
+    Friend WithEvents cmdsamadengan As System.Windows.Forms.Button
+    Friend WithEvents cmdbutton1 As System.Windows.Forms.Button
+    Friend WithEvents cmdbutton2 As System.Windows.Forms.Button
+    Friend WithEvents cmdbutton3 As System.Windows.Forms.Button
+    Friend WithEvents cmdbutton0 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Ferry_Hidayat/latihan_1_ferry/latihan_1_ferry/Form1.vb
@@ -0,0 +1,166 @@
+﻿Public Class KALKULATOR
+
+    Public Class KALKULATOR
+        Friend Hasil As Single = 0
+        Dim Operasi As String = ""
+        Dim OperasiHitung As Boolean = False
+    End Class
+
+    Private Property OperasiHitung As Boolean
+
+    Private Property Operasi As String
+
+    Private Property Hasil As Integer
+
+
+
+    Private Sub KALKULATOR_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        OperasiHitung = False
+        Operasi = ""
+        Hasil = 0
+        txtlayar.Text = "0"
+    End Sub
+
+    Private Sub cmdbutton1_Click(sender As Object, e As EventArgs) Handles cmdbutton1.Click
+        If OperasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "1"
+            OperasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "1"
+        End If
+    End Sub
+
+    Private Sub cmdbutton2_Click(sender As Object, e As EventArgs) Handles cmdbutton2.Click
+        If OperasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "2"
+            OperasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "2"
+        End If
+    End Sub
+
+    Private Sub cmdbutton3_Click(sender As Object, e As EventArgs) Handles cmdbutton3.Click
+        If OperasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "3"
+            OperasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "3"
+        End If
+    End Sub
+
+
+    Private Sub cmdbutton4_Click(sender As Object, e As EventArgs) Handles cmdbutton4.Click
+        If OperasiHitung() = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "4"
+            OperasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "4"
+        End If
+    End Sub
+
+    Private Sub cmdbutton5_Click(sender As Object, e As EventArgs) Handles cmdbutton5.Click
+        If OperasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "5"
+            OperasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "5"
+        End If
+    End Sub
+
+    Private Sub cmdbutton6_Click(sender As Object, e As EventArgs) Handles cmdbutton6.Click
+        If OperasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "6"
+            OperasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "6"
+        End If
+    End Sub
+
+    Private Sub cmdbutton7_Click(sender As Object, e As EventArgs) Handles cmdbutton7.Click
+        If OperasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "7"
+            OperasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "7"
+        End If
+    End Sub
+
+    Private Sub cmdbutton8_Click(sender As Object, e As EventArgs) Handles cmdbutton8.Click
+        If OperasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "8"
+            OperasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "8"
+        End If
+    End Sub
+
+    Private Sub cmdbutton9_Click(sender As Object, e As EventArgs) Handles cmdbutton9.Click
+        If OperasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "9"
+            OperasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "9"
+        End If
+    End Sub
+
+    Private Sub cmdbutton0_Click(sender As Object, e As EventArgs) Handles cmdbutton0.Click
+        If OperasiHitung = True Or txtlayar.Text = "0" Then
+            txtlayar.Text = "0"
+            OperasiHitung = False
+        Else
+            txtlayar.Text = txtlayar.Text + "0"
+        End If
+    End Sub
+
+    Private Sub cmdtambah_Click(sender As Object, e As EventArgs) Handles cmdtambah.Click
+        Operasi = "tambah"
+        Hasil = Val(txtlayar.Text)
+        OperasiHitung = True
+    End Sub
+
+    Private Sub cmdkurang_Click(sender As Object, e As EventArgs) Handles cmdkurang.Click
+        Operasi = "kurang"
+        Hasil = Val(txtlayar.Text)
+        OperasiHitung = True
+    End Sub
+
+    Private Sub cmdkali_Click(sender As Object, e As EventArgs) Handles cmdkali.Click
+        Operasi = "kali"
+        Hasil = Val(txtlayar.Text)
+        OperasiHitung = True
+    End Sub
+
+    Private Sub cmdbagi_Click(sender As Object, e As EventArgs) Handles cmdbagi.Click
+        Operasi = "bagi"
+        Hasil = Val(txtlayar.Text)
+        OperasiHitung = True
+    End Sub
+
+    Private Sub panah_Click(sender As Object, e As EventArgs) Handles panah.Click
+        Operasi = "pangkat"
+        Hasil = Val(txtlayar.Text)
+        OperasiHitung = True
+    End Sub
+
+    Private Sub cmdsamadengan_Click(sender As Object, e As EventArgs) Handles cmdsamadengan.Click
+        Select Case Operasi
+            Case "tambah"
+                Hasil = Hasil + Val(txtlayar.Text)
+            Case "kurang"
+                Hasil = Hasil - Val(txtlayar.Text)
+            Case "kali"
+                Hasil = Hasil * Val(txtlayar.Text)
+            Case "bagi"
+                Hasil = Hasil / Val(txtlayar.Text)
+            Case "pangkat"
+                Hasil = Hasil ^ Val(txtlayar.Text)
+        End Select
+        txtlayar.Text = Hasil
+        OperasiHitung = True
+    End Sub
+
+    Private Sub cmdhapus_Click(sender As Object, e As EventArgs) Handles cmdhapus.Click
+        txtlayar.Text = "0"
+        OperasiHitung = True
+    End Sub
+End Class
