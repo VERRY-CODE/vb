ad67767d31de4f0d4c51622da86f5f243a27ae6d
FERRYHIDAYAT <145472042+VERRY-CODE@users.noreply.github.com>
2024-10-11 09:31:47 +0700

    Add files via upload

---
+++ pertemuan_2/ppn/ppn/Form1.Designer.vb
@@ -0,0 +1,307 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txtbaso = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.txtes = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.cmdhasil = New System.Windows.Forms.Button()
+        Me.txttotal = New System.Windows.Forms.TextBox()
+        Me.txtppn = New System.Windows.Forms.TextBox()
+        Me.txtjumlah = New System.Windows.Forms.TextBox()
+        Me.txtchoco = New System.Windows.Forms.TextBox()
+        Me.txtkentang = New System.Windows.Forms.TextBox()
+        Me.txtayam = New System.Windows.Forms.TextBox()
+        Me.txtnasi = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(201, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(434, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Form Pajak Pertambahan NIlai Pada MD"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel1.Controls.Add(Me.txtbaso)
+        Me.Panel1.Controls.Add(Me.Label11)
+        Me.Panel1.Controls.Add(Me.txtes)
+        Me.Panel1.Controls.Add(Me.Label10)
+        Me.Panel1.Controls.Add(Me.cmdhasil)
+        Me.Panel1.Controls.Add(Me.txttotal)
+        Me.Panel1.Controls.Add(Me.txtppn)
+        Me.Panel1.Controls.Add(Me.txtjumlah)
+        Me.Panel1.Controls.Add(Me.txtchoco)
+        Me.Panel1.Controls.Add(Me.txtkentang)
+        Me.Panel1.Controls.Add(Me.txtayam)
+        Me.Panel1.Controls.Add(Me.txtnasi)
+        Me.Panel1.Controls.Add(Me.Label9)
+        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Location = New System.Drawing.Point(12, 63)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(803, 670)
+        Me.Panel1.TabIndex = 1
+        '
+        'txtbaso
+        '
+        Me.txtbaso.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtbaso.Location = New System.Drawing.Point(189, 376)
+        Me.txtbaso.Name = "txtbaso"
+        Me.txtbaso.Size = New System.Drawing.Size(234, 29)
+        Me.txtbaso.TabIndex = 36
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(99, 381)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(67, 24)
+        Me.Label11.TabIndex = 35
+        Me.Label11.Text = "BASO "
+        '
+        'txtes
+        '
+        Me.txtes.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtes.Location = New System.Drawing.Point(189, 300)
+        Me.txtes.Name = "txtes"
+        Me.txtes.Size = New System.Drawing.Size(234, 29)
+        Me.txtes.TabIndex = 34
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(82, 300)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(84, 24)
+        Me.Label10.TabIndex = 33
+        Me.Label10.Text = "ES TEH "
+        '
+        'cmdhasil
+        '
+        Me.cmdhasil.Location = New System.Drawing.Point(702, 535)
+        Me.cmdhasil.Name = "cmdhasil"
+        Me.cmdhasil.Size = New System.Drawing.Size(83, 29)
+        Me.cmdhasil.TabIndex = 32
+        Me.cmdhasil.Text = "HASIL"
+        Me.cmdhasil.UseVisualStyleBackColor = True
+        '
+        'txttotal
+        '
+        Me.txttotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txttotal.Location = New System.Drawing.Point(457, 597)
+        Me.txttotal.Name = "txttotal"
+        Me.txttotal.Size = New System.Drawing.Size(239, 29)
+        Me.txttotal.TabIndex = 31
+        '
+        'txtppn
+        '
+        Me.txtppn.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtppn.Location = New System.Drawing.Point(457, 535)
+        Me.txtppn.Name = "txtppn"
+        Me.txtppn.Size = New System.Drawing.Size(239, 29)
+        Me.txtppn.TabIndex = 30
+        '
+        'txtjumlah
+        '
+        Me.txtjumlah.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtjumlah.Location = New System.Drawing.Point(457, 476)
+        Me.txtjumlah.Name = "txtjumlah"
+        Me.txtjumlah.Size = New System.Drawing.Size(234, 29)
+        Me.txtjumlah.TabIndex = 29
+        '
+        'txtchoco
+        '
+        Me.txtchoco.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtchoco.Location = New System.Drawing.Point(190, 221)
+        Me.txtchoco.Name = "txtchoco"
+        Me.txtchoco.Size = New System.Drawing.Size(234, 29)
+        Me.txtchoco.TabIndex = 28
+        '
+        'txtkentang
+        '
+        Me.txtkentang.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtkentang.Location = New System.Drawing.Point(189, 157)
+        Me.txtkentang.Name = "txtkentang"
+        Me.txtkentang.Size = New System.Drawing.Size(234, 29)
+        Me.txtkentang.TabIndex = 27
+        '
+        'txtayam
+        '
+        Me.txtayam.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtayam.Location = New System.Drawing.Point(189, 94)
+        Me.txtayam.Name = "txtayam"
+        Me.txtayam.Size = New System.Drawing.Size(234, 29)
+        Me.txtayam.TabIndex = 26
+        '
+        'txtnasi
+        '
+        Me.txtnasi.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtnasi.Location = New System.Drawing.Point(189, 34)
+        Me.txtnasi.Name = "txtnasi"
+        Me.txtnasi.Size = New System.Drawing.Size(234, 29)
+        Me.txtnasi.TabIndex = 25
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(411, 600)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(40, 24)
+        Me.Label9.TabIndex = 24
+        Me.Label9.Text = "RP."
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(199, 600)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(196, 24)
+        Me.Label8.TabIndex = 23
+        Me.Label8.Text = "TOTAL DIBAYARKAN"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(363, 540)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(88, 24)
+        Me.Label7.TabIndex = 22
+        Me.Label7.Text = "PPN 10%"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(366, 479)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(85, 24)
+        Me.Label6.TabIndex = 21
+        Me.Label6.Text = "JUMLAH"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(23, 224)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(161, 24)
+        Me.Label5.TabIndex = 20
+        Me.Label5.Text = "CHOCHO FLOAT"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(82, 160)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(102, 24)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "KENTANG"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(34, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(150, 24)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "FIELD CHICKEN"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(132, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 24)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "NASI"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(827, 765)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents cmdhasil As System.Windows.Forms.Button
+    Friend WithEvents txttotal As System.Windows.Forms.TextBox
+    Friend WithEvents txtppn As System.Windows.Forms.TextBox
+    Friend WithEvents txtjumlah As System.Windows.Forms.TextBox
+    Friend WithEvents txtchoco As System.Windows.Forms.TextBox
+    Friend WithEvents txtkentang As System.Windows.Forms.TextBox
+    Friend WithEvents txtayam As System.Windows.Forms.TextBox
+    Friend WithEvents txtnasi As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtes As System.Windows.Forms.TextBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents txtbaso As System.Windows.Forms.TextBox
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+
+End Class
---
+++ pertemuan_2/ppn/ppn/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+
+    Private Property Harga As String
+
+
+    Private Sub txtchoco_TextChanged(sender As Object, e As EventArgs) Handles txtchoco.TextChanged
+        txtjumlah.Text = Val(txtnasi.Text) + Val(txtayam.Text) + Val(txtkentang.Text) + Val(txtchoco.Text)
+    End Sub
+
+    Private Sub cmdhasil_Click(sender As Object, e As EventArgs) Handles cmdhasil.Click
+
+        'mendeklarasikan variabel constanta PPN 10%'
+        Const PPN As Single = 0.1
+        Dim Harga As Single
+
+
+        'untuk menghitung harga PPN 10%'
+        Harga = txtjumlah.Text
+        txtppn.Text = Harga * PPN
+
+        'untuk menghitung jumlah yang akan dibayarkan dari hasil jumah + potongan PPN'
+        txttotal.Text = Harga + Val(txtppn.Text)
+    End Sub
+
+    Private Sub txtkentang_TextChanged(sender As Object, e As EventArgs) Handles txtkentang.TextChanged
+        txtjumlah.Text = Val(txtnasi.Text) + Val(txtayam.Text) + Val(txtkentang.Text) + Val(txtchoco.Text)
+    End Sub
+
+    Private Sub txtnasi_TextChanged(sender As Object, e As EventArgs) Handles txtnasi.TextChanged
+        txtjumlah.Text = Val(txtnasi.Text) + Val(txtayam.Text) + Val(txtkentang.Text) + Val(txtchoco.Text) + Val(txtes.Text) + Val(txtbaso.Text)
+    End Sub
+
+
+    Private Sub txtayam_TextChanged(sender As Object, e As EventArgs) Handles txtayam.TextChanged
+        txtjumlah.Text = Val(txtnasi.Text) + Val(txtayam.Text) + Val(txtkentang.Text) + Val(txtchoco.Text) + Val(txtes.Text) + Val(txtbaso.Text)
+    End Sub
+
+    Private Sub txtes_TextChanged(sender As Object, e As EventArgs) Handles txtes.TextChanged
+        txtjumlah.Text = Val(txtnasi.Text) + Val(txtayam.Text) + Val(txtkentang.Text) + Val(txtchoco.Text) + Val(txtes.Text) + Val(txtbaso.Text)
+    End Sub
+
+
+    Private Sub txtbaso_TextChanged(sender As Object, e As EventArgs) Handles txtbaso.TextChanged
+        txtjumlah.Text = Val(txtnasi.Text) + Val(txtayam.Text) + Val(txtkentang.Text) + Val(txtchoco.Text) + Val(txtes.Text) + Val(txtbaso.Text)
+    End Sub
+End Class
