4d551c9725860c21da2e11a09e727ce8d0e822a6
FERRYHIDAYAT <145472042+VERRY-CODE@users.noreply.github.com>
2024-10-18 09:18:12 +0700

    Add files via upload

---
+++ pertemuan_3/WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,416 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        Panel2 = New Panel()
+        Label13 = New Label()
+        txthuruf = New TextBox()
+        txtrata_rata = New TextBox()
+        Label14 = New Label()
+        Label12 = New Label()
+        Label11 = New Label()
+        Label10 = New Label()
+        Label9 = New Label()
+        Label8 = New Label()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        cmdhasil = New Button()
+        TextBox6 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
+        txtpresensi = New TextBox()
+        txtkeaktifan = New TextBox()
+        txtpersentasi = New TextBox()
+        txttugas = New TextBox()
+        txtuas = New TextBox()
+        txtuts = New TextBox()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.Bisque
+        Panel1.Controls.Add(Panel2)
+        Panel1.Controls.Add(Label14)
+        Panel1.Controls.Add(Label12)
+        Panel1.Controls.Add(Label11)
+        Panel1.Controls.Add(Label10)
+        Panel1.Controls.Add(Label9)
+        Panel1.Controls.Add(Label8)
+        Panel1.Controls.Add(Label7)
+        Panel1.Controls.Add(Label6)
+        Panel1.Controls.Add(Label5)
+        Panel1.Controls.Add(Label4)
+        Panel1.Controls.Add(Label3)
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(cmdhasil)
+        Panel1.Controls.Add(TextBox6)
+        Panel1.Controls.Add(TextBox5)
+        Panel1.Controls.Add(TextBox4)
+        Panel1.Controls.Add(TextBox3)
+        Panel1.Controls.Add(TextBox2)
+        Panel1.Controls.Add(TextBox1)
+        Panel1.Controls.Add(txtpresensi)
+        Panel1.Controls.Add(txtkeaktifan)
+        Panel1.Controls.Add(txtpersentasi)
+        Panel1.Controls.Add(txttugas)
+        Panel1.Controls.Add(txtuas)
+        Panel1.Controls.Add(txtuts)
+        Panel1.Location = New Point(26, 16)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(1064, 856)
+        Panel1.TabIndex = 0
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.LightSalmon
+        Panel2.Controls.Add(Label13)
+        Panel2.Controls.Add(txthuruf)
+        Panel2.Controls.Add(txtrata_rata)
+        Panel2.Location = New Point(255, 738)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(635, 85)
+        Panel2.TabIndex = 57
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Font = New Font("Segoe UI", 12F)
+        Label13.Location = New Point(12, 28)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(199, 28)
+        Label13.TabIndex = 58
+        Label13.Text = "Rata-Rata Nilai Anda "
+        ' 
+        ' txthuruf
+        ' 
+        txthuruf.Font = New Font("Segoe UI", 12F)
+        txthuruf.Location = New Point(443, 28)
+        txthuruf.Name = "txthuruf"
+        txthuruf.Size = New Size(164, 34)
+        txthuruf.TabIndex = 57
+        ' 
+        ' txtrata_rata
+        ' 
+        txtrata_rata.Font = New Font("Segoe UI", 12F)
+        txtrata_rata.Location = New Point(217, 28)
+        txtrata_rata.Name = "txtrata_rata"
+        txtrata_rata.Size = New Size(195, 34)
+        txtrata_rata.TabIndex = 56
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Font = New Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label14.Location = New Point(168, 9)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(722, 46)
+        Label14.TabIndex = 56
+        Label14.Text = "FROM PERHITUNGAN NILAI MATA KULIAH "
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI", 12F)
+        Label12.Location = New Point(611, 569)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(62, 28)
+        Label12.TabIndex = 54
+        Label12.Text = "(10%)"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI", 12F)
+        Label11.Location = New Point(611, 486)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(62, 28)
+        Label11.TabIndex = 53
+        Label11.Text = "(10%)"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI", 12F)
+        Label10.Location = New Point(611, 398)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(62, 28)
+        Label10.TabIndex = 52
+        Label10.Text = "(15%)"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI", 12F)
+        Label9.Location = New Point(611, 311)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(62, 28)
+        Label9.TabIndex = 51
+        Label9.Text = "(25%)"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 12F)
+        Label8.Location = New Point(611, 228)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(62, 28)
+        Label8.TabIndex = 50
+        Label8.Text = "(20%)"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 12F)
+        Label7.Location = New Point(611, 137)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(62, 28)
+        Label7.TabIndex = 49
+        Label7.Text = "(20%)"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 12F)
+        Label6.Location = New Point(194, 572)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(88, 28)
+        Label6.TabIndex = 48
+        Label6.Text = "persensi "
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 12F)
+        Label5.Location = New Point(186, 486)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(96, 28)
+        Label5.TabIndex = 47
+        Label5.Text = "keaktifan "
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 12F)
+        Label4.Location = New Point(115, 398)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(167, 28)
+        Label4.TabIndex = 46
+        Label4.Text = "persentasi project"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 12F)
+        Label3.Location = New Point(155, 314)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(132, 28)
+        Label3.TabIndex = 45
+        Label3.Text = "tugas project "
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F)
+        Label2.Location = New Point(98, 228)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(189, 28)
+        Label2.TabIndex = 44
+        Label2.Text = "ujian akhir semester "
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 12F)
+        Label1.Location = New Point(80, 137)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(207, 28)
+        Label1.TabIndex = 43
+        Label1.Text = "ujian tengah semester "
+        ' 
+        ' cmdhasil
+        ' 
+        cmdhasil.BackColor = Color.DeepSkyBlue
+        cmdhasil.Font = New Font("Segoe UI", 12F)
+        cmdhasil.Location = New Point(751, 640)
+        cmdhasil.Name = "cmdhasil"
+        cmdhasil.Size = New Size(115, 60)
+        cmdhasil.TabIndex = 42
+        cmdhasil.Text = "HASIL"
+        cmdhasil.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Font = New Font("Segoe UI", 12F)
+        TextBox6.Location = New Point(696, 566)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(164, 34)
+        TextBox6.TabIndex = 39
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Font = New Font("Segoe UI", 12F)
+        TextBox5.Location = New Point(699, 483)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(164, 34)
+        TextBox5.TabIndex = 38
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Font = New Font("Segoe UI", 12F)
+        TextBox4.Location = New Point(699, 398)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(164, 34)
+        TextBox4.TabIndex = 37
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Font = New Font("Segoe UI", 12F)
+        TextBox3.Location = New Point(699, 311)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(164, 34)
+        TextBox3.TabIndex = 36
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Segoe UI", 12F)
+        TextBox2.Location = New Point(696, 228)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(164, 34)
+        TextBox2.TabIndex = 35
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 12F)
+        TextBox1.Location = New Point(699, 134)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(161, 34)
+        TextBox1.TabIndex = 34
+        ' 
+        ' txtpresensi
+        ' 
+        txtpresensi.Font = New Font("Segoe UI", 12F)
+        txtpresensi.Location = New Point(303, 569)
+        txtpresensi.Name = "txtpresensi"
+        txtpresensi.Size = New Size(277, 34)
+        txtpresensi.TabIndex = 33
+        ' 
+        ' txtkeaktifan
+        ' 
+        txtkeaktifan.Font = New Font("Segoe UI", 12F)
+        txtkeaktifan.Location = New Point(303, 483)
+        txtkeaktifan.Name = "txtkeaktifan"
+        txtkeaktifan.Size = New Size(277, 34)
+        txtkeaktifan.TabIndex = 32
+        ' 
+        ' txtpersentasi
+        ' 
+        txtpersentasi.Font = New Font("Segoe UI", 12F)
+        txtpersentasi.Location = New Point(303, 398)
+        txtpersentasi.Name = "txtpersentasi"
+        txtpersentasi.Size = New Size(277, 34)
+        txtpersentasi.TabIndex = 31
+        ' 
+        ' txttugas
+        ' 
+        txttugas.Font = New Font("Segoe UI", 12F)
+        txttugas.Location = New Point(303, 311)
+        txttugas.Name = "txttugas"
+        txttugas.Size = New Size(277, 34)
+        txttugas.TabIndex = 30
+        ' 
+        ' txtuas
+        ' 
+        txtuas.Font = New Font("Segoe UI", 12F)
+        txtuas.Location = New Point(303, 228)
+        txtuas.Name = "txtuas"
+        txtuas.Size = New Size(277, 34)
+        txtuas.TabIndex = 29
+        ' 
+        ' txtuts
+        ' 
+        txtuts.Font = New Font("Segoe UI", 12F)
+        txtuts.Location = New Point(303, 134)
+        txtuts.Name = "txtuts"
+        txtuts.Size = New Size(277, 34)
+        txtuts.TabIndex = 28
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1115, 895)
+        Controls.Add(Panel1)
+        Name = "Form1"
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cmdhasil As Button
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtpresensi As TextBox
+    Friend WithEvents txtkeaktifan As TextBox
+    Friend WithEvents txtpersentasi As TextBox
+    Friend WithEvents txttugas As TextBox
+    Friend WithEvents txtuas As TextBox
+    Friend WithEvents txtuts As TextBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents txthuruf As TextBox
+    Friend WithEvents txtrata_rata As TextBox
+
+End Class
---
+++ pertemuan_3/WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form1
+    Private Sub txtuts_TextChanged(sender As Object, e As EventArgs) Handles txtuts.TextChanged
+
+        Const UTS As Single = 0.2
+        Dim Rata_uts As Single
+
+        Rata_uts = txtuts.Text
+
+        TextBox1.Text = UTS * Rata_uts
+
+    End Sub
+
+    Private Sub txtuas_TextChanged(sender As Object, e As EventArgs) Handles txtuas.TextChanged
+        Const UAS As Single = 0.2
+        Dim Rata_uas As Single
+
+        Rata_uas = txtuas.Text
+
+        TextBox2.Text = UAS * Rata_uas
+    End Sub
+
+    Private Sub txttugas_TextChanged(sender As Object, e As EventArgs) Handles txttugas.TextChanged
+        Const TUGAS As Single = 0.25
+        Dim Rata_tugas As Single
+
+        Rata_tugas = txttugas.Text
+
+        TextBox3.Text = TUGAS * Rata_tugas
+    End Sub
+
+    Private Sub txtpersentasi_TextChanged(sender As Object, e As EventArgs) Handles txtpersentasi.TextChanged
+        Const PERSENTASI As Single = 0.15
+        Dim Rata_persentasi As Single
+
+        Rata_persentasi = txtpersentasi.Text
+        TextBox4.Text = PERSENTASI * Rata_persentasi
+
+    End Sub
+
+    Private Sub txtkeaktifan_TextChanged(sender As Object, e As EventArgs) Handles txtkeaktifan.TextChanged
+        Const KEAKTIFAN As Single = 0.1
+        Dim Rata_keaktifan As Single
+
+        Rata_keaktifan = txtkeaktifan.Text
+        TextBox5.Text = KEAKTIFAN * Rata_keaktifan
+    End Sub
+
+    Private Sub txtpresensi_TextChanged(sender As Object, e As EventArgs) Handles txtpresensi.TextChanged
+        Const PERSENSI As Single = 0.1
+        Dim Rata_persensi As Single
+
+        Rata_persensi = txtpresensi.Text
+        TextBox6.Text = PERSENSI * Rata_persensi
+    End Sub
+End Class
